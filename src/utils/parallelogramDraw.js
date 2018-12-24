b25d1913e7f346c805e96f50308b140f581ecc4b
matheus92 <lima.eca@gmail.com>
2018-12-23 23:53:21 -0200

    testing function that generates 4th point of parallelogram

--- src/utils/parallelogramDraw.js
+++ src/utils/parallelogramDraw.js
@@ -1,25 +1,35 @@
 const define4thPoints = (P1, P2, P3) => {
   // defining angular coefficient
-  const m1 = (P3.coordinateY - P1.coordinateY) / (P3.coordinateX - P1.coordinateX);
-  const m2 = (P2.coordinateY - P1.coordinateY) / (P2.coordinateX - P1.coordinateX);
-  const m3 = (P3.coordinateY - P2.coordinateY) / (P3.coordinateX - P2.coordinateX);
+  const m1 = P3.coordinateX - P1.coordinateX !== 0
+    ? (P3.coordinateY - P1.coordinateY) / (P3.coordinateX - P1.coordinateX)
+    : (P3.coordinateY - P1.coordinateY) / 0.0001;
+  const m2 = P2.coordinateX - P1.coordinateX !== 0
+    ? (P2.coordinateY - P1.coordinateY) / (P2.coordinateX - P1.coordinateX)
+    : (P2.coordinateY - P1.coordinateY) / 0.0001;
+  const m3 = P3.coordinateX - P2.coordinateX !== 0
+    ? (P3.coordinateY - P2.coordinateY) / (P3.coordinateX - P2.coordinateX)
+    : (P3.coordinateY - P2.coordinateY) / 0.0001;
 
   // defining intersection points
   const Y1 = ((m1 * m2 * (P3.coordinateX - P2.coordinateX))
     + (m2 * P2.coordinateY)
     - (m1 * P3.coordinateY)) / (m2 - m1);
-  const X1 = ((m1 * P2.coordinateX) - (m2 * P3.coordinateX)) / (m1 - m2);
+  const X1 = ((m1 * P2.coordinateX) - (m2 * P3.coordinateX)
+    - P2.coordinateY + P3.coordinateY) / (m1 - m2);
 
   const Y2 = ((m2 * m3 * (P1.coordinateX - P3.coordinateX))
     + (m3 * P3.coordinateY)
     - (m2 * P1.coordinateY)) / (m3 - m2);
-  const X2 = ((m2 * P3.coordinateX) - (m3 * P1.coordinateX)) / (m2 - m3);
+  const X2 = ((m2 * P3.coordinateX) - (m3 * P1.coordinateX)
+    - P3.coordinateY + P1.coordinateY) / (m2 - m3);
 
   const Y3 = ((m1 * m3 * (P1.coordinateX - P2.coordinateX))
     + (m3 * P2.coordinateY)
     - (m1 * P1.coordinateY)) / (m3 - m1);
-  const X3 = ((m1 * P2.coordinateX) - (m3 * P1.coordinateX)) / (m1 - m3);
-
+  const X3 = ((m1 * P2.coordinateX) - (m3 * P1.coordinateX)
+    - P2.coordinateY + P1.coordinateY) / (m1 - m3);
+  // console.log('-----------------------');
+  // console.log(X1, Y1, X2, Y2, X3, Y3);
   return [
     { coordinateX: Math.round(X1), coordinateY: Math.round(Y1) },
     { coordinateX: Math.round(X2), coordinateY: Math.round(Y2) },
--- src/utils/parallelogramDraw.test.js
+++ src/utils/parallelogramDraw.test.js
@@ -1,10 +1,53 @@
 import define4thPoints from './parallelogramDraw';
 
+const P1 = { coordinateX: 0, coordinateY: 10 };
+const P2 = { coordinateX: 0, coordinateY: 0 };
+const P3 = { coordinateX: 10, coordinateY: 0 };
+
 describe('Parallelogram Draw Tests', () => {
-  // smoke tests
   describe('Smoke tests', () => {
     test('Function define4thPoints must exist', () => {
       expect(define4thPoints).toBeDefined();
+    });
+  });
+  describe('Defining the position of the 4th point of the parallelogram', () => {
+    describe(`If the position of 3 points (P1, P2, P3) of a parallelogram is known, we can define:
+    
+    -The equation of the line that passes through P1 and P2, that will be named r12.
+    -The equation of the line that passes through P1 and P3, that will be named r13.
+    -The equation of the line that passes through P2 and P3, that will be named r23.
+    
+    Now we can also define:
+    
+    -The equation of the line that is parallel to r23 and passes through P1, that will be named r1;
+    -The equation of the line that is parallel to r13 and passes through P2, that will be named r2;
+    -The equation of the line that is parallel to r12 and passes through P3. that will be named r3;
+    
+    By definition, in Euclidean geometry, a parallelogram is a simple (non-self-intersecting) 
+    quadrilateral with two pairs of parallel sides. Therefore, given P1, P2 and P3, 
+    we have 3 possible parallelograms:
+
+    -The first parallelogram has the 4th point positioned at the intersection of r3 and r2;
+    -The second parallelogram has the 4th point positioned at the intersection of r1 and r3;
+    -The third parallelogram has the 4th point positioned at the intersection of r2 and r1;
+    `, () => {
+      test(`
+        When:
+          -P1 is positioned at (0,10)
+          -P2 is positioned at (0,0)
+          -P3 is positioned at (10,0)
+        Then:
+          -The 4th point of the first parallelogram will be at (10, -10)
+          -The 4th point of the second parallelogram will be at (10, 10)
+          -The 4th point of the third parallelogram will be at (-10, 10)`, () => {
+        const positions4thPoints = define4thPoints(P1, P2, P3);
+        expect(positions4thPoints[0].coordinateX).toBe(10);
+        expect(positions4thPoints[0].coordinateY).toBe(-10);
+        expect(positions4thPoints[1].coordinateX).toBe(10);
+        expect(positions4thPoints[1].coordinateY).toBe(10);
+        expect(positions4thPoints[2].coordinateX).toBe(-10);
+        expect(positions4thPoints[2].coordinateY).toBe(10);
+      });
     });
   });
 });
